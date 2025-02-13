c983aea36c88486d1220ea7b3fd829c8385c6df1
italanleal <italanleal@gmail.com>
2025-02-13 20:15:10 +0000

    aCommit

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/italanleal/go-array-utils/utils"
-)
-
 func main() {
-	nums := []int{1, 2, 4, 5}
-
-	fmt.Println(nums)
-	fmt.Println(utils.Reverse(nums))
-	fmt.Println(utils.Map(nums, func(value int) int {
-		return (value * 2)
-	}))
-	fmt.Println(utils.Reduce(nums, func(acm int, cur int) int {
-		return (acm + cur)
-	}, 0))
 }
