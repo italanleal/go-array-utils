0b9b3f211876ef2c361b44ff8816ac7c895aeba9
italanleal <italanleal@gmail.com>
2025-02-14 23:45:42 +0000

    aCommit

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,39 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/italanleal/go-array-utils/utils"
+)
+
 func main() {
+	// Exemplo de uso da função Map
+	numbers := []int{1, 2, 3, 4, 5}
+	squared := utils.Map(numbers, func(n int) int {
+		return n * n
+	})
+	fmt.Println("Squared numbers:", squared)
+
+	// Exemplo de uso da função Reduce
+	sum := utils.Reduce(numbers, func(acc, cur int) int {
+		return acc + cur
+	}, 0)
+	fmt.Println("Sum of numbers:", sum)
+
+	// Exemplo de uso da função ForEach
+	fmt.Println("Printing numbers:")
+	utils.ForEach(numbers, func(n int) {
+		fmt.Println(n)
+	})
+
+	// Exemplo de uso da função Reverse
+	reversed := utils.Reverse(numbers)
+	fmt.Println("Reversed numbers:", reversed)
+
+	// Exemplo de uso da função Filter
+	evenNumbers := utils.Filter(numbers, func(n int) bool {
+		return n%2 == 0
+	})
+	fmt.Println("Even numbers:", evenNumbers)
+
 }
--- utils/array.go
+++ utils/array.go
@@ -1,5 +1,26 @@
 package utils
 
+/*
+Map aplica uma função fn a cada elemento do slice arr e retorna um novo slice contendo os resultados.
+
+Parâmetros:
+
+- arr: Slice de elementos do tipo T.
+
+- fn: Função que recebe um elemento de tipo T e retorna um valor de tipo R.
+
+Retorno:
+
+- Um slice contendo os valores resultantes da aplicação de fn a cada elemento de arr.
+
+Exemplo de uso:
+
+	numeros := []int{1, 2, 3, 4}
+	dobrados := Map(numeros, func(n int) int {
+		return n * 2
+	})
+	// dobrados agora contém [2, 4, 6, 8]
+*/
 func Map[T any, R any](arr []T, fn func(T) R) []R {
 	var result []R
 	for _, value := range arr {
@@ -8,6 +29,29 @@ func Map[T any, R any](arr []T, fn func(T) R) []R {
 	return result
 }
 
+/*
+Reduce aplica uma função de redução fn a cada elemento do slice arr, acumulando os resultados em um valor único.
+
+Parâmetros:
+
+- arr: Slice de elementos do tipo T.
+
+- fn: Função que recebe um acumulador (acc) e o elemento atual (cur), retornando o novo valor acumulado.
+
+- start: Valor inicial do acumulador.
+
+Retorno:
+
+- O valor final acumulado após a aplicação da função fn a todos os elementos de arr.
+
+Exemplo de uso:
+
+	numeros := []int{1, 2, 3, 4}
+	soma := Reduce(numeros, func(acc, cur int) int {
+		return acc + cur
+	}, 0)
+	// soma agora contém 10 (1 + 2 + 3 + 4)
+*/
 func Reduce[T any](arr []T, fn func(acc T, cur T) T, start T) T {
 	var result T = start
 	for _, value := range arr {
@@ -16,12 +60,54 @@ func Reduce[T any](arr []T, fn func(acc T, cur T) T, start T) T {
 	return result
 }
 
+/*
+ForEach percorre todos os elementos do slice arr e aplica a função fn a cada um deles.
+
+Parâmetros:
+
+- arr: Slice de elementos do tipo T.
+
+- fn: Função que recebe um elemento de tipo T e executa uma operação sobre ele, sem retorno.
+
+Retorno:
+
+- Nenhum retorno (void).
+
+Exemplo de uso:
+
+	numeros := []int{1, 2, 3, 4}
+	ForEach(numeros, func(n int) {
+		fmt.Println(n)
+	})
+	// Saída:
+	// 1
+	// 2
+	// 3
+	// 4
+*/
 func ForEach[T any](arr []T, fn func(T)) {
 	for _, value := range arr {
 		fn(value)
 	}
 }
 
+/*
+Reverse inverte a ordem dos elementos em um slice e retorna um novo slice com os elementos na ordem reversa.
+
+Parâmetros:
+
+- arr: Slice de elementos do tipo T a ser invertido.
+
+Retorno:
+
+- Um novo slice contendo os elementos de arr na ordem inversa.
+
+Exemplo de uso:
+
+	numeros := []int{1, 2, 3, 4, 5}
+	invertido := Reverse(numeros)
+	// invertido agora contém [5, 4, 3, 2, 1]
+*/
 func Reverse[T any](arr []T) []T {
 	var result []T
 
@@ -32,6 +118,27 @@ func Reverse[T any](arr []T) []T {
 	return result
 }
 
+/*
+Filter filtra os elementos de um slice com base em uma função condicional e retorna um novo slice contendo apenas os elementos que atendem ao critério especificado.
+
+Parâmetros:
+
+- arr: Slice de elementos do tipo T a ser filtrado.
+
+- fn: Função que recebe um elemento de T e retorna um booleano indicando se o elemento deve ser incluído no resultado.
+
+Retorno:
+
+- Um novo slice contendo apenas os elementos de arr que satisfazem a condição definida em fn.
+
+Exemplo de uso:
+
+	numeros := []int{1, 2, 3, 4, 5}
+	pares := Filter(numeros, func(n int) bool {
+		return n%2 == 0
+	})
+	// pares agora contém [2, 4]
+*/
 func Filter[T any](arr []T, fn func(T) bool) []T {
 	var result []T
 
